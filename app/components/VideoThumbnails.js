b9453fc81f2e7490256f4764af1746b645fc13eb
Ibxdevgh <ibxprofile@gmail.com>
2025-09-18 18:25:00 +0530

    Add files via upload

--- app/components/VideoThumbnails.js
+++ app/components/VideoThumbnails.js
@@ -85,8 +85,6 @@ export default function VideoThumbnail({ videoId }) {
             <iframe
               className="w-[90vw] aspect-video rounded-lg shadow-xl mx-8"
               src={`https://player.mux.com/jzsCr4zl5wDyQmjdg24hDWwaUeEo3AwIkG4NjPZ5IL00`}
-              title="YouTube video player"
-              frameBorder="0"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               allowFullScreen
             ></iframe>
