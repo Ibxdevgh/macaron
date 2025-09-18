e50c319752a7f8f3eebe4a7460d569b535de931a
Ibxdevgh <ibxprofile@gmail.com>
2025-09-18 16:19:09 +0530

    Add files via upload

--- app/components/VideoThumbnails.js
+++ app/components/VideoThumbnails.js
@@ -87,7 +87,6 @@ export default function VideoThumbnail({ videoId }) {
             <iframe
               className="w-[90vw] aspect-video rounded-lg shadow-xl mx-8"
               src={`https://player.mux.com/jzsCr4zl5wDyQmjdg24hDWwaUeEo3AwIkG4NjPZ5IL00`}
-              style="width: 100%; border: none; aspect-ratio: 16/9;"
               allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
               allowFullScreen
             ></iframe>
